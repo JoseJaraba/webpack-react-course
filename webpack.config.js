fa469368df1ac2b22bfe1d73515c371511406c5c
JoseJaraba <josem.jaraba@hotmail.com>
2022-07-29 12:01:35 -0500

    Class04/Setting up css on react

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -26,6 +27,14 @@ module.exports = {
                         loader: 'html-loader'
                     }
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -34,6 +43,9 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
+        new MiniCssExtractPlugin({
+            filename:'[name].css'
+        })
     ],
     devServer: {
         static: {
