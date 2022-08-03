e5a78b7d2f08110d772eaefc8bbfe89228385263
JoseJaraba <josem.jaraba@hotmail.com>
2022-08-03 16:14:53 -0500

    Class09/React context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,32 +12,56 @@ import { CreateTodoButtom } from "./CreateTodoButtom";
 // ];
 
 const useLocalStorage = (itemName, initialValue) => {
-    const localStorageItem =localStorage.getItem(itemName);
-    let parsedItem;
+    const [item, setItem] = React.useState(initialValue);
+    const [dataStatus,setDataStatus] = useState({loading:true,error:false})
 
-    if (!localStorageItem) {
-        localStorage.setItem(itemName, JSON.stringify(initialValue));
-        parsedItem = initialValue;
-    } else {
-        parsedItem = JSON.parse(localStorageItem);
-    }
 
-    const [item, setItem] = React.useState(parsedItem);
+    React.useEffect(() => {
+        setTimeout(() => {
+            try {
+                const localStorageItem =localStorage.getItem(itemName);
+                let parsedItem;
+
+                if (!localStorageItem) {
+                    localStorage.setItem(itemName, JSON.stringify(initialValue));
+                    parsedItem = initialValue;
+                } else {
+                    parsedItem = JSON.parse(localStorageItem);
+                }
+
+                setItem(parsedItem);
+                setDataStatus({...dataStatus,loading:false});
+
+            } catch (error) {
+                setDataStatus({...dataStatus,error:true})
+            }
+
+        }, 1000);
+    }, []);
 
     const saveItem = (newItem) => {
-        const stringifiedItem = JSON.stringify(newItem);
-        localStorage.setItem(itemName, stringifiedItem);
-        setItem(newItem);
+        try {
+            const stringifiedItem = JSON.stringify(newItem);
+            localStorage.setItem(itemName, stringifiedItem);
+            setItem(newItem);
+        } catch (error) {
+            setDataStatus({...dataStatus,error:true})
+        }
     }
 
-    return [
+    return {
         item,
-        saveItem
-    ];
+        saveItem,
+        dataStatus
+    };
 }
 
 const App = () => {
-    const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        dataStatus
+    } = useLocalStorage('TODOS_V1', []);
     const [searchTaskValue, setSearchTaskValue] = React.useState('');
 
     const completedTodos = todos.filter(todo => todo.completed).length;
@@ -68,12 +92,19 @@ const App = () => {
         ));
     }
 
+    React.useEffect(() => {
+        console.log('use effect');
+    },[]);
+
     return (
         // only one tag must be returned 
         <React.Fragment>
             <TodoCounter total={totalTodos} completed={completedTodos}/>
             <TodoSearch searchTaskValue={searchTaskValue} setSearchTaskValue={setSearchTaskValue} />
             <TodoList>
+                {dataStatus.error && <p>Error...</p>}
+                {dataStatus.loading && <p>Loading...</p>}
+                {(!dataStatus.loading && !searchedTodos.length) && <p>¡Created your first TODO!</p>}
                 {searchedTodos.map(todo => (
                     <TodoItem key={todo.text} text={todo.text} completed={todo.completed} onComplete={() => toggleCompleteTodo(todo.text)} onDelete={() => deleteTodo(todo.text)} />
                 ))}
